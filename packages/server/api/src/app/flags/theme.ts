2455903ce8e1a0a4bb89dc05bd90f12240293b90
Wathek Al Zoubi <wathek.z@hotmail.com>
2025-05-27 02:11:57 +0300

    Update theme.ts

--- packages/server/api/src/app/flags/theme.ts
+++ packages/server/api/src/app/flags/theme.ts
@@ -65,7 +65,7 @@ export function generateTheme({
 
 export const defaultTheme = generateTheme({
     primaryColor: '#6e41e2',
-    websiteName: '222',
+    websiteName: 'CloudifyTechs',
     fullLogoUrl: 'https://logosbynick.com/wp-content/uploads/2018/03/final-logo-example.png',
     favIconUrl: 'https://cdn.activepieces.com/brand/favicon.ico',
     logoIconUrl: 'https://cdn.activepieces.com/brand/logo.svg',
